7e9e5fc7b960d68e4ba484dc2e8cfbc5163e4dae
Sebastien Dors <dorssebastien@gmail.com>
2025-03-10 14:25:15 +0100

    Jdbc Template working - SELECT * FROM map request

--- src/main/java/com/epf/Main.java
+++ src/main/java/com/epf/Main.java
@@ -1,7 +1,21 @@
 package com.epf;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.epf.Persistance.ConfigBDD;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigBDD.class);
+        JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+        
+        String sql = "SELECT * FROM map";
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+        for (Map<String, Object> row : rows) {
+            System.out.println(row);
+        }
     }
 }
